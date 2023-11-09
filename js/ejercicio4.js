237fd9ba56d29509c10ab72cba91f7d867ec501f
SebastianVillagra <sebastianvilla16.com@gmail.com>
2023-11-09 14:25:33 -0300

    agrego ejercicio 4

---
+++ js/ejercicio4.js
@@ -0,0 +1,21 @@
+/**Escribir el código de una función a la que se pasa como parámetro un número
+entero y devuelve como resultado una cadena de texto que indica si el número es
+par o impar. Mostrar por pantalla el resultado devuelto por la función.*/
+
+function parOImpar(numero) {
+    if (numero % 2 === 0) {
+      return "El número ingresado es par";
+    } else {
+      return "El número ingresado es impar";
+    }
+  }
+  
+  var numero = parseInt(prompt("Ingrese un numero entero:"));
+  
+  if (!isNaN(numero)) {
+    var resultado = parOImpar(numero);
+    document.write(resultado);
+  } else {
+    document.write("Numero invalido.");
+  }
+  
