b70f8d7724690334bac6b66b9ace22b716b15051
SebastianVillagra <sebastianvilla16.com@gmail.com>
2023-11-09 14:31:40 -0300

    agrego ejercicio 5

---
+++ js/ejercicio5.js
@@ -0,0 +1,24 @@
+/**Deﬁnir una función que muestre información sobre una cadena de texto que se
+le pasa como argumento. A partir de la cadena que se le pasa, la función
+determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o
+por una mezcla de ambas.*/
+
+function analizar(cadena) {
+    if (cadena === cadena.toUpperCase()) {
+      return "La cadena contiene solo letras mayusculas";
+    }
+    if (cadena === cadena.toLowerCase()) {
+      return "La cadena contiene solo letras mayusculas";
+    }
+    return "La cadena es una mezcla, contiene letras mayusculas y minusculas";
+  }
+  
+  var cadena = prompt("Ingrese una cadena de texto:");
+  
+  if (cadena !== null) {
+    var resultado = analizar(cadena);
+    document.write(resultado);
+  } else {
+    document.write("Se ingreso una cadena invalida");
+  }
+  
